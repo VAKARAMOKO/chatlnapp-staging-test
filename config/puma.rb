1b76a60ed5a4632664142c29c6cbbbea01602b41
VAKARAMOKO <vakaramokokamate93@gmail.com>
2019-06-19 17:27:10 +0000

    @prod_config: add config puma and procfile

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+#port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -39,14 +39,18 @@ environment ENV.fetch("RAILS_ENV") { "development" }
  
 
 rackup      DefaultRackup
-#port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 
 
 on_worker_boot do
+  # Valid on Rails up to 4.1 the initializer method of setting `pool` size
   ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
+    ActiveRecord::Base.establish_connection(config)
   end
 end
 # Allow puma to be restarted by `rails restart` command.
